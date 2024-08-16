2bd53391ab48281d3118386471279ec473842704
Beatriz <111025459+bia3112@users.noreply.github.com>
2024-08-15 21:51:04 -0300

    arquivo

---
+++ mission-pet/src/main/java/br/unipar/missionpet/MissionPetApplication.java
@@ -0,0 +1,13 @@
+package br.unipar.missionpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MissionPetApplication.class, args);
+    }
+
+}
---
+++ mission-pet/src/test/java/br/unipar/missionpet/MissionPetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unipar.missionpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
